7ac4fc6fa4c204f15c1f721bd7f1ab53bd6315b1
Jeff Jiang <jeff.jiang13@gmail.com>
2024-02-13 01:41:46 -0500

    readME added

--- app/(dashboard)/(routes)/music/page.tsx
+++ app/(dashboard)/(routes)/music/page.tsx
@@ -87,7 +87,7 @@ const MusicPage = () => {
                     <Input
                       className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
                       disabled={isLoading}
-                      placeholder="Guitar solo"
+                      placeholder="Piano solo"
                       {...field}
                     />
                   </FormControl>
