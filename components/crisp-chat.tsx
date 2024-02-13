d36f937031385ef42a19b638b78c60bbfee66f61
Jeff Jiang <jeff.jiang13@gmail.com>
2024-02-13 00:58:15 -0500

    readME added

--- components/crisp-chat.tsx
+++ components/crisp-chat.tsx
@@ -5,7 +5,7 @@ import { Crisp } from "crisp-sdk-web";
 
 export const CrispChat = () => {
   useEffect(() => {
-    Crisp.configure("8254f25a-eca0-421e-b4de-2bf655c03887");
+    Crisp.configure("9214823e-ac23-4ba1-b93e-7f1c7cf0e625");
   }, []);
 
   return null;
--- components/landing-content.tsx
+++ components/landing-content.tsx
@@ -4,25 +4,25 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 const testimonials = [
   {
-    name: "Joel",
+    name: "AJ",
     avatar: "J",
     title: "Software Engineer",
     description: "This is the best application I've ever used!",
   },
   {
-    name: "Antonio",
+    name: "Danny",
     avatar: "A",
     title: "Designer",
     description: "I use this daily for generating new photos!",
   },
   {
-    name: "Mark",
+    name: "Jack",
     avatar: "M",
     title: "CEO",
     description: "This app has changed my life, cannot imagine working without it!",
   },
   {
-    name: "Mary",
+    name: "Jen",
     avatar: "M",
     title: "CFO",
     description: "The best in class, definitely worth the premium subscription!",
