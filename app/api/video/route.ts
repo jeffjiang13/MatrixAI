fea084ac2ed1a8ddae7d75eb8f787dfb79f0c36b
Jeff Jiang <jeff.jiang13@gmail.com>
2024-02-13 02:26:57 -0500

    readME added

--- app/api/video/route.ts
+++ app/api/video/route.ts
@@ -33,7 +33,7 @@ export async function POST(
     }
 
     const response = await replicate.run(
-      "anotherjesse/zeroscope-v2-xl:9f747673945c62801b13b84701c783929c0ee784e4748ec062204894dda1a351",
+      "anotherjesse/zeroscope-v2-xl:71996d331e8ede8ef7bd76eba9fae076d31792e4ddf4ad057779b443d6aea62f",
       {
         input: {
           prompt,
