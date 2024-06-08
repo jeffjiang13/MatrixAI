fd991a5c06b5be6a814d88d40f057a50ffa6b765
Jeff Jiang <jeff.jiang13@gmail.com>
2024-06-08 16:26:40 -0400

    first commit

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/", "/api/webhook"],
+  publicRoutes: ["/", "/api/webhook",'/pricing','/documentation','/overview','/contact'],
 });
 
 export const config = {
