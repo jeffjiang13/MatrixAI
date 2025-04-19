e182aafb6a5a1886611f46baf551cef1e9a7b12c
Jeff Jiang <jeff.jiang13@gmail.com>
2025-04-19 18:21:15 -0400

    updated clerk

--- components/loader.tsx
+++ components/loader.tsx
@@ -11,7 +11,7 @@ export const Loader = () => {
         />
       </div>
       <p className="text-sm text-muted-foreground">
-        Matrixing...
+        NextGening...
       </p>
     </div>
   );
